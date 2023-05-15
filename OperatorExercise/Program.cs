b35f4e2a52a46370392395a9e9399b658b7b9d2e
Chinedu Obinnah~ <cobinnah@yahoo.com>
2023-05-15 10:23:20 -0500

    Operator Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AreaofCircle();
+
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} = {quotient} remainder {remainder}");
+            }
+        }
+        public static void AreaofCircle()
+        {
+            Console.Write("What is the radius of your circle? ");
+            double rad = Convert.ToDouble(Console.ReadLine());
+            double area = Math.PI * rad * rad;
+            Console.WriteLine("Area of circle is: " + area);
         }
     }
 }
